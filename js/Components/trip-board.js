3ad89cdd1f6858ed19371ad724ff942bbbf9463a
DESKTOP-TEOCFN4\Usuario <jeroenvhaasteren@gmail.com>
2017-04-26 00:19:11 +0200

    Board is dynamic

--- js/Components/trip-board.js
+++ js/Components/trip-board.js
@@ -2,28 +2,66 @@
  * Created by Jeroen on 15/04/2017.
  */
 Vue.component('trip-board', {
+    props: ['items'],
     template: `
         <!-- trip-board -->
         <div id="Board">
+        
+            <div class="toolbar">
+                <div v-show="!modalNewItem" > <i class="material-icons" @click="modalNewItem = true">add_box</i> </div>
+                <div v-show="modalNewItem" class="addNewContainer" >
+                    <div class="input-field">
+                      <input id="newUrl" v-model="newUrl" type="text" class="validate">
+                      <label for="newUrl">Url</label>
+                    </div>
+                    <div class="btn-container"><a class="waves-effect waves-light btn" @click="addNewItem">Add</a></div>
+                    <div class="btn-container"><a class="waves-effect waves-light grey lighten-2 btn" @click="modalNewItem = false">Cancel</a></div>
+                </div>
+            </div>
+            
 
-            <article class="trip-item z-depth-1">
+            <article class="trip-item z-depth-1" v-for="(item, index) in m_Items">
                 <div class="trip-item-icons">
                     <ul>
                         <li><a><i class="material-icons">share</i></a></li>
-                        <li class="disabled"><a><i class="material-icons">euro_symbol</i></a></li>
-                        <li><a><i class="material-icons">comment</i></a></li>
-                        <li><a><i class="material-icons">favorite</i></a></li>
+                        <li :class=" item.financials ? null : 'disabled'" @click="openFinancials(item.id)"><a><i class="material-icons">euro_symbol</i></a></li>
+                        <li :class=" item.comments < 1 ? 'disabled' : null" @click="openItem(item.id)"><a><i class="material-icons">comment</i></a></li>
+                        <li @click="toggleLiked(index)"><a><i class="material-icons" v-if="item.liked">favorite</i><i class="material-icons" v-else>favorite_border</i></a></li>
                     </ul>
                 </div>
-                <div class="trip-item-img">
-                    <img src="rescources/houseGaudi.jpg">
+                <div class="trip-item-img" @click="openItem(item.id)">
+                    <img :src="item.img">
                 </div>
-                <div class="trip-item-details">
-                    <h3>Casa Batllo</h3>
-                    <p>Web oficial del edificio modernista de Antoni Gaudí en Barcelona Casa Batlló. Información sobre Gaudí y el edificio, visitas, precios, ofertas y eventos.</p>
+                <div class="trip-item-details" @click="openItem(item.id)">
+                    <h3>{{item.title}}</h3>
+                    <p>{{item.desc}}</p>
                 </div>
             </article>
 
         </div>
-    `
+    `,
+    data: function() {
+        return {
+            m_Items: this.items,
+            newUrl: '',
+            modalNewItem: false
+        }
+    },
+    methods:{
+        openItem: function(id) {
+            window.EventChannel.$emit('openDetailsItem', id);
+        },
+        openFinancials: function(id) {
+            window.EventChannel.$emit('openFinancialsItem', id);
+        },
+        toggleLiked: function(index) {
+            this.m_Items[index].liked ? this.m_Items[index].liked = false : this.m_Items[index].liked = true;
+        },
+        addNewItem: function() {
+            window.EventChannel.$emit('addNewItem', this.newUrl);
+            this.newUrl = '';
+            this.modalNewItem = false;
+
+        }
+    }
 });
--- js/Components/trip-item.js
+++ js/Components/trip-item.js
@@ -2,89 +2,175 @@
  * Created by Jeroen on 15/04/2017.
  */
 Vue.component('trip-item', {
+    props: ['item'],
     template: `
         <!-- trip-item -->
         <div id="trip-item">
-            <div class="row">
-                <div class="col s8" id="Details">
-                    <h5>Details</h5>
-                    <form class="col s12">
-                        <div class="row">
-                            <div class="input-field col s6">
-                                <input id="tripTitle" type="text" class="validate">
-                                <label for="tripTitle">Item title</label>
-                            </div>
-                            <div class="input-field col s6">
-                                <input id="Location" type="text" class="validate">
-                                <label for="Location">Price</label>
-                            </div>
+            <div class="comments">
+                <div class="message-field">
+                    
+                    
+                    
+                    <div class="message left z-depth-1">
+                        <div class="title">
+                            Pedro
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12 padding">
-                                <textarea id="Description" class="materialize-textarea"></textarea>
-                                <label for="Description">Description</label>
-                            </div>
+                        <div class="text">
+                            Lorem ipsum and more way more bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
                         </div>
-                        <div class="row">
-                            <div class="input-field col s6">
-                                <input type="date" id="formDate" class="datepicker">
-                                <label for="formDate">Date</label>
-                            </div>
-                            <div class="input-field col s6">
-                                <input type="date" id="tillDate" class="datepicker">
-                                <label for="tillDate">Time</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col m6">
-                                <input id="tripTitle" type="text" class="validate">
-                                <label for="tripTitle">Location</label>
-                            </div>
-                            <div class="input-field col m6">
-                                Google maps
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="col s4" id="comments">
-                    <h5>Comments</h5>
-                    <div class="messages">
-                        <div class="message right">
-                            <div class="bubble z-depth-1">
-                                <p>Hello i have a message about this item i think we could better buy tickets before hand if we want to go there</p>
-                            </div>
-                            <span class="sender">Jeroen van Haasteren - 16:56 15 april 2017</span>
-                        </div>
-                        <div class="message left">
-                            <div class="bubble z-depth-1">
-                                <p>Hello i have a message about this item i think we could better buy tickets before hand if we want to go there</p>
-                            </div>
-                            <span class="sender">Jeroen van Haasteren - 16:56 15 april 2017</span>
-                        </div>
-                        <div class="message left">
-                            <div class="bubble z-depth-1">
-                                <p>Hello i have a message about this item i think we could better buy tickets before hand if we want to go there</p>
-                            </div>
-                            <span class="sender">Jeroen van Haasteren - 16:56 15 april 2017</span>
-                        </div>
-                        <div class="message right">
-                            <div class="bubble z-depth-1">
-                                <p>Hello i have a message about this item i think we could better buy tickets before hand if we want to go there</p>
-                            </div>
-                            <span class="sender">Jeroen van Haasteren - 16:56 15 april 2017</span>
+                        <div class="Stamp">
+                            05/03/17
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <textarea id="textarea1" class="materialize-textarea"></textarea>
-                            <label for="textarea1">Type Message</label>
+        
+                    <div class="message right z-depth-1">
+                        <div class="text">
+                            Lorem ipsum and more way more bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
                         </div>
-                        <div style="text-align: right">
-                            <a class="waves-effect waves-light btn"><i class="material-icons left">send</i>Send</a>
+                        <div class="Stamp">
+                            05/03/17
                         </div>
+                    </div>
+        
+                </div>
+                <div class="type-container">
+                    <div class="input-container">
+                        <textarea></textarea>
+                    </div>
+                    <div class="send-container">
+                        <a class="waves-effect waves-light btn"><i class="material-icons">send</i></a>
                     </div>
                 </div>
             </div>
+            <div class="details">
+                <div class="title">
+                    <img src="rescources/houseGaudi.jpg" class="z-depth-1">
+                    <div style="width: 100%; padding: 0 10px">
+                        <div class="input-field">
+                            <input id="Title" type="text" value="Casa Batllo Barcelona">
+                            <label for="Title">Title</label>
+                        </div>
+                        <div class="input-field">
+                            <textarea id="Desc" class="materialize-textarea" style="font-size: 12px; max-height: 68px;"> Web oficial del edificio modernista de Antoni Gaudí en Barcelona Casa Batlló. Información sobre Gaudí y el edificio, visitas, precios, ofertas y eventos."</textarea>
+                            <label for="Desc">Description</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="toolbar">
+                    <div class="tools" title="Likes">
+                        3 <i class="material-icons">favorite_border</i>
+                    </div>
+                    <div class="tools" title="Website">
+                        <i class="material-icons">link</i>
+                    </div>
+                    <div class="tools" title="Share">
+                        <i class="material-icons">share</i>
+                    </div>
+                    <div class="tools" title="Add payment">
+                        <i class="material-icons">attach_money</i>
+                    </div>
+                </div>
+                <div style="display: flex;">
+                    <div style="flex: 1; padding-left: 15px;">
+                        <div class="input-field">
+                            <input id="Price" type="text">
+                            <label for="Price">Price</label>
+                        </div>
+                    </div>
+                    <div style="flex: 1; padding-left: 15px;">
+                        <div class="input-field">
+                            <input type="checkbox" class="filled-in" id="Payed" checked="checked" />
+                            <label for="Payed">Payed</label>
+                        </div>
+                    </div>
+                </div>
+                <div style="display: flex;">
+                    <div style="flex: 1; padding-left: 15px;">
+                        <div class="input-field">
+                            <input type="date" id="StartDate" class="datepicker">
+                            <label for="StartDate">Start date</label>
+                        </div>
+                    </div>
+                    <div style="flex: 1; padding-left: 15px;">
+                        <div class="input-field">
+                            <input type="date" id="EndDate" class="datepicker">
+                            <label for="EndDate">End date</label>
+                        </div>
+                    </div>
+                </div>
+                <div style="display: flex;">
+                    <div style="flex: 1; padding-left: 15px;">
+                        <div class="switch">
+                            <label>
+                                Single day
+                                <input type="checkbox">
+                                <span class="lever"></span>
+                                Multiple days
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div style="display: flex;">
+                    <div style="flex: 1; padding-left: 15px; padding-top: 10px;">
+                        Tags:
+                        <div class="chips chips-autocomplete"></div>
+                    </div>
+                </div>
+                <div style="display: flex;">
+                    <div style="flex: 1; padding-left: 15px; padding-top: 10px;">
+                        <div class="input-field">
+                            <input id="Location" type="text" class="">
+                            <label for="Location">Location</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="MapsContainer">
+                    <img src="rescources/GoogleMaps.png">
+                </div>
+            </div>
         </div>
-    `
+    `,
+    data: function() {
+        return {
+            m_Item: window.defaultItem,
+            m_message: []
+        }
+    },
+    watch: {
+        item: function() {
+            this.m_Item = $.extend(window.defaultItem, this.item)
+            this.getMessages();
+        }
+    },
+    methods: {
+        getMessages: function() {
+            //this.m_Item.id
+            this.m_message = window.mockMessage[0].messages;
+        }
+    }
 });
+
+var defaultItem = {
+    id: 0,
+    img: '',
+    title: '',
+    desc: '',
+    comments: 0,
+    financials: false,
+    likes: 0,
+    liked: false,
+    price: '',
+    f_date: '',
+    t_date: '',
+    time: '',
+    tags: [],
+    location: ''
+}
+
+var mockMessage = [
+    {messages:[
+        {sender:'Pedro',message:'I think this is awesome we could look for more things like this',date:'01/04/17'},
+        {sender:'Hilda',message:'Yes that looked so awesome i would love to do that',date:'02/04/17'},
+        {message:'I found a website with a lot of other websites where we can get more information about this',date:'02/04/17'}
+        ]}
+]
