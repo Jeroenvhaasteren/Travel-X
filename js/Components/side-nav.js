3a2d2a6b8d2c75a884e4faa689b6f27d0dd33961
Jeroen van Haasteren <jeroenvhaasteren@gmail.com>
2017-06-29 19:25:48 +0200

    Css styling

--- js/Components/side-nav.js
+++ js/Components/side-nav.js
@@ -5,40 +5,27 @@
 Vue.component('side-nav', {
     template: `
         <!-- SideMenu -->
-        <aside id="mainMenu" class="z-depth-3">
-            <div class="logo-container">
+        <aside id="mainMenu">
+            <div class="menu-header">
                 <ul>
                     <li class="menu-item">
-                        <img src="rescources/cross-grey.png">
+                        Menu
                     </li>
                 </ul>
             </div>
             <div class="menu-item-container">
                 <ul>
                     <li class="menu-item hover">
-                        <img src="rescources/cross-grey.png">
                         My trips
                     </li>
                     <li class="menu-item hover">
-                        <img src="rescources/cross-grey.png">
                         Barcelona 2017
                     </li>
                     <li class="menu-item hover">
-                        <img src="rescources/cross-grey.png">
                         Favorites
                     </li>
                     <li class="menu-item hover">
-                        <img src="rescources/cross-grey.png">
                         Inspiration
-                    </li>
-                </ul>
-            </div>
-            <div class="profile-container">
-                <ul>
-                    <li class="menu-item">
-                        <img src="rescources/cross-grey.png">
-                        <span id="profileSettings" class="smallIcon"><i class="material-icons">settings</i></span>
-                        <span id="profileNotifications" class="smallIcon"><i class="material-icons">inbox</i></span>
                     </li>
                 </ul>
             </div>
